97afb2e882e64a95df6c38767e1329b5577944d4
Christopher Anderton <christopher@deluxive.se>
2014-09-26 22:12:46 +0200

    Primary commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+=== Matbloggspriset Dashboard News Widget WP ===
+Contributors: Christopher Anderton
+Tags: feed, wordpress, matbloggspriset, nyheter, dashboard
+Requires at least: 3.6
+Stable tag: 1.0.0
+Tested up to: 4.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+Matbloggspriset Dashboard News Widget är en väldigt enkel panel-widget för WordPress. Den visar de senaste nyheterna från Matbloggspriset.se
+
+
+
+== Installation ==
+
+1. Ladda ner denna plugin från: https://github.com/matbloggspriset/matbloggspriset-dashboard-feeds-wp/archive/master.zip
+2. Packa upp zip-filen och ladda upp den till WordPress folder för plugins (wp-content/plugins/)
+3. Gå till “Tillägg” i den vänstra sidmenyn på din bloggs admin-meny.
+4. Välj “Installerade tillägg”.
+5. I listan så finns det “Matbloggspriset Dashboard Feeds” listat. Under titeln så trycker du på länken där det står “Aktivera”.
+
+Du kan nu se de senaste nyheterna från Matbloggspriset genom att klicka på “Panel” i admin-menyn.
+
+== Changelog ==
+
+= 1.0.0 =
+* Första släppet
---
+++ matbloggspriset-dashboard-feeds.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name: Matbloggspriset Dashboard Feeds
+Plugin URI: http://matbloggspriset
+Description: Visar Matbloggsprisets senaste nyheter i din WordPress-Panel / Shows the latest news from The Swedish Food Blog Awards in your Dashboard.
+Author: Christopher Anderton
+Version: 1.0.0
+Tags: widget, dashboard, feed, matbloggspriset
+Author URI: http://matbloggspriset.se
+Text Domain: matbloggspriset-dashboard-feeds
+Domain Path: /languages/
+License: GPLv2 or later
+*/
+
+/*
+Copyright 2014 Christopher Anderton, Matbloggspriset
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+function wp_admin_dashboard_add_news_feed_widget() {
+global $wp_meta_boxes;
+wp_add_dashboard_widget( 'dashboard_matbloggspriset_feed', __("Nyheter från Matbloggspriset", "matbloggspriset-dashboard-feeds"), 'dashboard_matbloggspriset_feed_output' );
+}
+add_action('wp_dashboard_setup', 'wp_admin_dashboard_add_news_feed_widget');
+function dashboard_matbloggspriset_feed_output() {
+echo '<div class="matbloggspriset-feed">';
+wp_widget_rss_output(array(
+'url' => 'http://matbloggspriset.se/feed/',
+'title' => __('Visar nyheter från Matbloggspriset i din WordPress-Panel', 'matbloggspriset-dashboard-feeds'),
+'items' => 5,
+'show_summary' => 0,
+'show_author' => 1,
+'show_date' => 1
+));
+echo '<p><hr>' . __('Visar senaste nytt från <a target=_blank" href="http://matbloggspriset.se">matbloggspriset.se</a> i din WordPress-Panel','matbloggspriset-dashboard-feeds') . '</p>';
+echo "</div>";
+}
+// End //
+function matbloggspriset_action_init() {
+load_plugin_textdomain('matbloggspriset-dashboard-feeds', false, dirname(plugin_basename(__FILE__) ) . '/languages/' );
+}
+add_action('init', 'matbloggspriset_action_init');
+?>
